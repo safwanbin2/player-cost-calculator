e14f8256773f49d632dbf66f6655e75ab23f3363
Safwan <safwanridwan321@gmail.com>
2022-08-22 11:26:51 +0600

    done with the top v section

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 document.getElementById('player').addEventListener('click', function(event){
-    const btn = event.target.innerText;
-    if(btn === 'Select'){
+    const btn = event.target;
+    const card = event.target;
+    const playerName = card.previousElementSibling.innerText;
+    if(btn.innerText === 'Select'){
+        
+        // getting ol and creating li inside it with the value from cardName
         const list = document.getElementById('ol');
         const newLi = document.createElement('li');
-        newLi.innerText = 'name';
+        newLi.innerText = playerName;
         list.appendChild(newLi);
+
+        // disabling
+        btn.setAttribute('disabled', '');
+        btn.style.backgroundColor = 'grey';
+        btn.style.cursor = 'none';
     }
     else{
-        
+        return 0;
     }
 });
